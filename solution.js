bb084b9cf7b5bfb44f9d11c1441a47bedff61862
Vruz1103 <vruz2001@gmail.com>
2024-09-06 17:51:54 +0530

    added all the learnings of the topics

--- solution.js
+++ solution.js
@@ -1,95 +1,95 @@
-//tic tac toe
-let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector("#reset-btn");
-let newGameBtn = document.querySelector("#new-btn");
-let msgContainer = document.querySelector(".msg-container");
-let msg = document.querySelector("#msg");
-let count = 0;
+// //tic tac toe
+// let boxes = document.querySelectorAll(".box");
+// let resetBtn = document.querySelector("#reset-btn");
+// let newGameBtn = document.querySelector("#new-btn");
+// let msgContainer = document.querySelector(".msg-container");
+// let msg = document.querySelector("#msg");
+// let count = 0;
 
-let turn0 = true;
+// let turn0 = true;
 
-const winPatterns = [
-  [0, 1, 2],
-  [0, 3, 6],
-  [0, 4, 8],
-  [1, 4, 7],
-  [2, 5, 8],
-  [2, 4, 6],
-  [3, 4, 5],
-  [6, 7, 8],
-];
+// const winPatterns = [
+//   [0, 1, 2],
+//   [0, 3, 6],
+//   [0, 4, 8],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [2, 4, 6],
+//   [3, 4, 5],
+//   [6, 7, 8],
+// ];
 
-boxes.forEach((box) => {
-  box.addEventListener("click", () => {
-    // console.log("box was clicked");
-    if (turn0) {
-      box.innerText = "0";
-      turn0 = false;
-    } else {
-      box.innerText = "X";
-      turn0 = true;
-    }
-    box.disabled = true;
-    count++;
+// boxes.forEach((box) => {
+//   box.addEventListener("click", () => {
+//     // console.log("box was clicked");
+//     if (turn0) {
+//       box.innerText = "0";
+//       turn0 = false;
+//     } else {
+//       box.innerText = "X";
+//       turn0 = true;
+//     }
+//     box.disabled = true;
+//     count++;
 
-    let isWinner = checkWinner();
+//     let isWinner = checkWinner();
 
-    if (count === 9 && !isWinner) {
-      gameDraw();
-    }
-  });
-});
+//     if (count === 9 && !isWinner) {
+//       gameDraw();
+//     }
+//   });
+// });
 
-const gameDraw = () => {
-  msg.innerText = `Game was Draw`;
-  msgContainer.classList.remove("hide");
-  disableBoxes();
-};
+// const gameDraw = () => {
+//   msg.innerText = `Game was Draw`;
+//   msgContainer.classList.remove("hide");
+//   disableBoxes();
+// };
 
-const disableBoxes = () => {
-  for (let box of boxes) {
-    box.disabled = true;
-  }
-};
+// const disableBoxes = () => {
+//   for (let box of boxes) {
+//     box.disabled = true;
+//   }
+// };
 
-const enableBoxes = () => {
-  for (let box of boxes) {
-    box.disabled = false;
-    box.innerText = "";
-  }
-};
+// const enableBoxes = () => {
+//   for (let box of boxes) {
+//     box.disabled = false;
+//     box.innerText = "";
+//   }
+// };
 
-const showWinner = (winner) => {
-  msg.innerText = `congratulations, the winner is ${winner}`;
-  msgContainer.classList.remove("hide");
-  disableBoxes();
-};
+// const showWinner = (winner) => {
+//   msg.innerText = `congratulations, the winner is ${winner}`;
+//   msgContainer.classList.remove("hide");
+//   disableBoxes();
+// };
 
-const checkWinner = () => {
-  for (let pattern of winPatterns) {
-    let pos1Val = boxes[pattern[0]].innerText;
-    let pos2Val = boxes[pattern[1]].innerText;
-    let pos3Val = boxes[pattern[2]].innerText;
+// const checkWinner = () => {
+//   for (let pattern of winPatterns) {
+//     let pos1Val = boxes[pattern[0]].innerText;
+//     let pos2Val = boxes[pattern[1]].innerText;
+//     let pos3Val = boxes[pattern[2]].innerText;
 
-    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
-      if (pos1Val === pos2Val && pos2Val === pos3Val) {
-        console.log("winner");
-        showWinner(pos2Val);
-        // boxes.forEach((box) => (box.disabled = true));
-        // return;
-      }
-    }
-  }
-};
+//     if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+//       if (pos1Val === pos2Val && pos2Val === pos3Val) {
+//         console.log("winner");
+//         showWinner(pos2Val);
+//         // boxes.forEach((box) => (box.disabled = true));
+//         // return;
+//       }
+//     }
+//   }
+// };
 
-const resetGame = () => {
-  turn0 = true;
-  enableBoxes();
-  msgContainer.classList.add("hide");
-};
+// const resetGame = () => {
+//   turn0 = true;
+//   enableBoxes();
+//   msgContainer.classList.add("hide");
+// };
 
-newGameBtn.addEventListener("click", resetGame);
-resetBtn.addEventListener("click", resetGame);
+// newGameBtn.addEventListener("click", resetGame);
+// resetBtn.addEventListener("click", resetGame);
 
 //stone paper scissors
 // let userScore = 0;
@@ -152,3 +152,169 @@ resetBtn.addEventListener("click", resetGame);
 //     playGame(userChoice);
 //   });
 // });
+
+// const employee = {
+//   calcTax1() {
+//     console.log("tax rate is 10%");
+//   },
+//   calctax2: function () {
+//     console.log("tax rate is 10%");
+//   },
+// };
+
+// const karanArjun = {
+//   salary: 50000,
+//   calcTax() {
+//     console.log("tax rate is 20%");
+//   },
+// };
+
+// const karanArjun2 = {
+//   salary: 50000,
+// };
+
+// const karanArjun3 = {
+//   salary: 50000,
+// };
+
+// const karanArjun4 = {
+//   salary: 50000,
+// };
+
+// karanArjun.__proto__ = employee;
+// karanArjun2.__proto__ = employee;
+// karanArjun3.__proto__ = employee;
+// karanArjun4.__proto__ = employee;
+
+// class ToyotaCar {
+//   constructor(brand, mileage) {
+//     console.log("create new object");
+//     this.brand = brand;
+//     this.mileage = mileage;
+//   }
+//   start() {
+//     console.log("start");
+//   }
+//   stop() {
+//     console.log("stop");
+//   }
+
+//   // setBrand(brand) {
+//   //   this.brandName = brand;
+//   // }
+// }
+
+// // let fortuner = new ToyotaCar();
+// // fortuner.setBrand("Fortuner");
+// // let lexus = new ToyotaCar();
+
+// let fortuner = new ToyotaCar("fortuner", 10);
+// console.log(fortuner);
+// let lexus = new ToyotaCar("lexus", 12);
+// console.log(lexus);
+
+// class Parent {
+//   hello() {
+//     console.log("hello");
+//   }
+// }
+
+// class Child extends Parent {}
+
+// let obj = new Child();
+
+// class Person {
+//   constructor(name) {
+//     this.species = "homo sapiens";
+//     this.name = name;
+//   }
+
+//   eat() {
+//     console.log("eat");
+//   }
+
+//   // sleep() {
+//   //   console.log("sleep");
+//   // }
+
+//   // work() {
+//   //   console.log("do nothing");
+//   // }
+// }
+
+// class Engineer extends Person {
+//   // constructor(branch) {
+//   //   super();
+//   //   this.branch = branch;
+//   // }
+//   constructor(name) {
+//     super(name);
+//   }
+//   work() {
+//     console.log("solve problems");
+//   }
+// }
+
+// class Doctor extends Person {
+//   work() {
+//     console.log("Cure people");
+//   }
+// }
+
+// let vrundaObj = new Engineer();
+// console.log(vrundaObj);
+
+// let engObj = new Engineer("Chemical");
+// console.log(engObj);
+
+// let engObj = new Engineer("Vrunda");
+// console.log(engObj);
+
+// let DATA = "secret info";
+
+// class User {
+//   constructor(name, email) {
+//     this.email = email;
+//     this.name = name;
+//   }
+
+//   viewData() {
+//     console.log(DATA);
+//   }
+// }
+
+// class Admin extends User {
+//   constructor(email,name){
+//     super(email,name);
+
+//   }
+//   editData() {
+//     DATA = "some new value";
+//   }
+// }
+
+// let student1 = new User("xyz@xmail.com", "xyz");
+// let student2 = new User("abc@amail.com", "abc");
+// let student3 = new User("lmn@lmail.com", "lmn");
+
+// let admin1 = new Admin();
+
+// function hello() {
+//   console.log("hello");
+// }
+
+// setTimeout(hello, 2000);
+
+// setTimeout(() => {
+//   console.log("hello");
+// }, 4000);
+
+function sum(a, b) {
+  console.log(a + b);
+}
+
+function calculator(a, b, sumCallback) {
+  sumCallback(a, b);
+}
+
+calculator(1, 2, sum);
